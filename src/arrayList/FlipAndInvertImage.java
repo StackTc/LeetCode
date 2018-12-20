4f777a166c10c0270682289ea7cf07d7a352fba8
StackTc <nishizhu12345>
2018-12-20 23:41:52 +0800

    :bug:20分钟解决，出现bug一次，数组越界问题

---
+++ src/arrayList/FlipAndInvertImage.java
@@ -0,0 +1,39 @@
+package arrayList;
+
+public class FlipAndInvertImage {
+
+    public static void main(String[] args) {
+
+        //Test [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+        //  [[1,1,0],[1,0,1],[0,0,0]]
+        int A[][] = {{1, 1,0,0}, {1, 0,0, 1}, {1, 0, 1,0}};
+        int B[][] = flipAndInvertImage(A);
+
+        for (int i = 0; i < B.length; i++) {
+            System.out.println();
+            for (int j = 0; j < B[i].length; j++) {
+                System.out.print(B[i][j] + ",");
+            }
+        }
+    }
+
+    private static int[][] flipAndInvertImage(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length / 2 ; j++) {
+                //水平反转
+                //[1,1,0]会产生[0,1,1]。
+                int temp = A[i][j];
+                A[i][j] = A[i][A[i].length - j - 1];
+                A[i][A[i].length - j - 1] = temp;
+
+            }
+
+            for (int j = 0; j < A[i].length; j++) {
+                //反转
+                //反转[0,1,1]会产生[1,0,0]。
+                A[i][j] = 1 - A[i][j];
+            }
+        }
+        return A;
+    }
+}
